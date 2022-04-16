ae8e0116339d59864f4b14f4098542b90264cc2d
Howard Akers <64698227+Cheeriesttomcat@users.noreply.github.com>
2022-04-16 19:54:05 -0400

    Update state.h

--- state.h
+++ state.h
@@ -28,18 +28,19 @@ struct IDEX {
     uint32_t read_data_1;
     uint32_t read_data_2;
     uint32_t sign_extended;
+    uint32_t opcode;
     //uint32_t rt;
     //uint32_t rd;
     // we only need the one write register
     uint32_t r_write; //I prefer write but this is consistent with previous name convention
     uint32_t funct_bits; // for alu op
-    uint32_t shamt;  
+    //uint32_t shamt;
     uint32_t jump_pc;
     uint32_t rs_num;
     uint32_t rt_num; // this is the register number we're writing to
     uint32_t rd_num;
-    
-    
+
+
 };
 
 // TODO:
@@ -47,17 +48,17 @@ struct IDEX {
 struct EXMEM {
     bool stall;
     control_t control;
-    int pc_adder;
-    bool alu_zero;
+    uint32_t pc_adder;
+    uint32_t alu_zero;
     uint32_t alu_result;
     uint32_t read_data_2;
     uint32_t r_write;
     uint32_t sign_extended;
     uint32_t funct_bits; // to check for loads etc.
-    
+
     uint32_t jump_pc;
     uint32_t pc_alu_result;
-    uint32_t write_num;
+    //uint32_t write_num; redundant
 };
 
 // TODO:
@@ -69,7 +70,7 @@ struct MEMWB {
     uint32_t alu_result;
     uint32_t r_write;
     uint32_t jal_reg;
-    uint32_t write_num;
+  //  uint32_t write_num; redundant
 };
 
 #endif
